c82d538ab2cc45bbb05f05a6d5b320dddd9ed58b
Alejandro Barrera <alejandro91dark@hotmail.es>
2022-08-23 16:14:39 -0400

    Se agrego funcion para seleccionar el sexo

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ let infoPeso = document.querySelector("#infoPeso");
 let altura = document.querySelector("#altura");
 let infoAltura = document.querySelector("#infoAltura");
 
+let sexMan = document.querySelector("#sex-man");
+let sexWoman = document.querySelector("#sex-woman");
+
+function selectMan(){
+    let sexo = "man";
+    console.log(sexo);
+    document.getElementById("sex-man").style.backgroundColor="#04AA6D";
+    document.getElementById("sex-woman").style.backgroundColor="rgba(0, 0, 0, 0)";
+    return sexo;
+}
+
+function selectWoman(){
+    let sexo = "woman";
+    console.log(sexo);
+    document.getElementById("sex-man").style.backgroundColor="rgba(0, 0, 0, 0)";
+    document.getElementById("sex-woman").style.backgroundColor="#04AA6D";
+    return sexo;
+}
 
 peso.oninput = () => {
     infoPeso.innerHTML = peso.value;
@@ -13,3 +31,5 @@ altura.oninput = () => {
     infoAltura.innerHTML = altura.value;
 }
 
+sexMan.onclick = selectMan;
+sexWoman.onclick = selectWoman;
