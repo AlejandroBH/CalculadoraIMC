1a1ed33fce37d99c028fa4b0bed0c50f0a9a7c22
Alejandro Barrera <alejandro91dark@hotmail.es>
2022-08-23 20:03:07 -0400

    Se creo funcion para calcular IMC y se asigno al respectivo btn

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-let peso = document.querySelector("#peso");
-let infoPeso = document.querySelector("#infoPeso");
-
-let altura = document.querySelector("#altura");
-let infoAltura = document.querySelector("#infoAltura");
-
+let weight = document.querySelector("#weight");
+let infoWeight = document.querySelector("#infoWeight");
+let height = document.querySelector("#height");
+let infoHeight = document.querySelector("#infoHeight");
 let sexMan = document.querySelector("#sex-man");
 let sexWoman = document.querySelector("#sex-woman");
+let infoTitle = document.querySelector(".info-title");
+let btnCalc = document.querySelector("#calculate-imc");
+let sex = "man";
+iconSex(1);
+
+function iconSex(select){
+    switch(select){
+        case 1:
+            document.getElementById("sex-man").style.backgroundColor="#04AA6D";
+            document.getElementById("sex-woman").style.backgroundColor="rgba(0, 0, 0, 0)";
+        break;
+        case 2:
+            document.getElementById("sex-man").style.backgroundColor="rgba(0, 0, 0, 0)";
+            document.getElementById("sex-woman").style.backgroundColor="#04AA6D";
+        break;
+    }
+}
 
 function selectMan(){
-    let sexo = "man";
-    console.log(sexo);
-    document.getElementById("sex-man").style.backgroundColor="#04AA6D";
-    document.getElementById("sex-woman").style.backgroundColor="rgba(0, 0, 0, 0)";
-    return sexo;
+    sex = "man";
+    console.log(sex);
+    iconSex(1);
+    return sex;
 }
 
 function selectWoman(){
-    let sexo = "woman";
-    console.log(sexo);
-    document.getElementById("sex-man").style.backgroundColor="rgba(0, 0, 0, 0)";
-    document.getElementById("sex-woman").style.backgroundColor="#04AA6D";
-    return sexo;
+    sex = "woman";
+    console.log(sex);
+    iconSex(2);
+    return sex;
 }
 
-peso.oninput = () => {
-    infoPeso.innerHTML = peso.value;
+function calcImc(){
+    let result = Math.round(weight.value / (height.value * height.value));
+
+    if(sex=="man"){
+        console.log(result + " man");
+        infoTitle.innerHTML = "Sexo Masculino";
+        return result;
+    }
+    if(sex=="woman"){
+        console.log(result + " woman");
+        infoTitle.innerHTML = "Sexo Femenino";
+        return result;
+    }
 }
 
-altura.oninput = () => {
-    infoAltura.innerHTML = altura.value;
+weight.oninput = () => {
+    infoWeight.innerHTML = weight.value;
 }
+
+height.oninput = () => {
+    infoHeight.innerHTML = height.value;
+}
+
+document.getElementById("information-on").style.display = "none";
 
 sexMan.onclick = selectMan;
 sexWoman.onclick = selectWoman;
+
+btnCalc.onclick = calcImc;
